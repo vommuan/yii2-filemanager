62fad492085ee8916b417d58465ac2de8ae321cd
Michael Naumov <vommuan@gmail.com>
2016-07-17 19:55:10 +0600

    Rename menu

--- messages/ru-RU/main.php
+++ messages/ru-RU/main.php
@@ -24,6 +24,7 @@ return [
     'Do resize thumbnails' => 'Сделать ресайз миниатюр',
     'Thumbnails sizes has been resized successfully!' => 'Размеры миниатюр успешно изменены!',
     'Do not forget every time you change thumbnails presets to make them resize.' => 'Не забывайте при каждом изменении предустановок миниатюр делать их ресайз.',
+    'Upload' => 'Загрузить',
     'Upload manager' => 'Менеджер загрузок',
     'Back to file manager' => 'Назад, к файловому менеджеру',
     'Alt attribute' => 'Атрибут alt',
