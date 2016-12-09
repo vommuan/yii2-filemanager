f60b547130391e5897ad7146a74e46d4c0b27b66
Michael Naumov <vommuan@gmail.com>
2016-12-09 12:49:59 +0700

    Bugfix. Owner's filter for last image on page

--- models/MediaFileSearch.php
+++ models/MediaFileSearch.php
@@ -17,14 +17,33 @@ class MediaFileSearch extends MediaFile
     const PAGE_SIZE = 30;
     
     /**
+     * Filter files by owner if it was configured
+     * 
+     * @param $query Link on Query object
+     */
+    protected function ownerFilter(&$query) {
+		if (Module::getInstance()->manageOwnFiles || (Module::getInstance()->rbac && !Yii::$app->user->can('filemanagerManageFiles') && Yii::$app->user->can('filemanagerManageOwnFiles'))) {
+			$query->joinWith('owner');
+			if (Yii::$app->user->isGuest) {
+				$query->andFilterWhere([Owner::tableName() . '.user_id' => 0]);
+			} else {
+				$query->andFilterWhere([Owner::tableName() . '.user_id' => Yii::$app->user->id]);
+			}
+		}
+	}
+    
+    /**
      * Get last file on page
      * 
      * @param integer $page
      * @return MediaFileSearch
      */
     public function searchLastOnPage($page) {
-		return self::find()->orderBy(['created_at' => SORT_DESC])
-			->offset($page * self::PAGE_SIZE - 1)->one(); // TODO: добавить права доступа на последний файл
+		$query = self::find()->orderBy(['created_at' => SORT_DESC]);
+		
+		$this->ownerFilter($query);
+		
+		return $query->offset($page * self::PAGE_SIZE - 1)->one();
 	}
     
     /**
@@ -44,14 +63,7 @@ class MediaFileSearch extends MediaFile
 			],
 		]);
 		
-		if (Module::getInstance()->manageOwnFiles || (Module::getInstance()->rbac && !Yii::$app->user->can('filemanagerManageFiles') && Yii::$app->user->can('filemanagerManageOwnFiles'))) {
-			$query->joinWith('owner');
-			if (Yii::$app->user->isGuest) {
-				$query->andFilterWhere([Owner::tableName() . '.user_id' => 0]);
-			} else {
-				$query->andFilterWhere([Owner::tableName() . '.user_id' => Yii::$app->user->id]);
-			}
-		}
+		$this->ownerFilter($query);
 		
 		return $dataProvider;
     }
