b2cfdfd604f7a260ba3b04e83a6ddeafea0d5217
Michael Naumov <vommuan@gmail.com>
2017-02-02 14:10:46 +0700

    Design was edited

--- widgets/views/file-manager/gallery__empty-text.php
+++ widgets/views/file-manager/gallery__empty-text.php
@@ -6,26 +6,29 @@ use yii\widgets\LinkPager;
 
 ?>
 
-<div class="gallery__summary">
-	<div class="summary">Показаны записи <b>0</b> из <b>0</b>.</div>
-</div>
-<div class="gallery__pager">
-	<?php
-	$this->beginBlock('empty-text-pagination', true);
-		echo LinkPager::widget(
-			array_merge(
-				$pagerParams, [
-					'pagination' => new Pagination([
-						'route' => '/' . Module::getInstance()->uniqueId . '/file/page',
-						'totalCount' => 1
-					]), // if point 0 intead of 1, pages not be displayed
-				]
-			)
-		);
-	$this->endBlock();
-	?>
-</div>
 <div class="gallery__items gallery-items" id="<?= $galleryItemsId;?>"></div>
+<div class="gallery__summary">
+	<div class="summary">
+		<?= Module::t(
+			'main',
+			'Showing files <b>{begin, number}-{end, number}</b> of <b>{totalCount, number}</b>.',
+			[
+				'begin' => 0,
+				'end' => 0,
+				'totalCount' => 0,
+			]
+		);?>
+	</div>
+</div>
 <div class="gallery__pager">
-	<?= $this->blocks['empty-text-pagination'];?>
+	<?= LinkPager::widget(
+		array_merge(
+			$pagerParams, [
+				'pagination' => new Pagination([
+					'route' => '/' . Module::getInstance()->uniqueId . '/file/page',
+					'totalCount' => 1
+				]), // if point 0 intead of 1, pages not be displayed
+			]
+		)
+	);?>
 </div>
--- widgets/views/file-manager/gallery__layout.php
+++ widgets/views/file-manager/gallery__layout.php
@@ -1,4 +1,3 @@
-<div class="gallery__summary">{summary}</div>
-<div class="gallery__pager">{pager}</div>
 <div class="gallery__items gallery-items" id="<?= $galleryItemsId;?>">{items}</div>
+<div class="gallery__summary">{summary}</div>
 <div class="gallery__pager">{pager}</div>
